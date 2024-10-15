a47a9ea5c2a99722f0c075de8d1e9525817eb7d7
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2024-10-15 17:14:51 +0300

    Fix Vercel Error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Set the base URL for the app
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
+  server: {
+    open: true, // Automatically open the app in the default browser
+  },
+});
